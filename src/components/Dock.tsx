31ec6ae1c34ff767543ce94d2d2a3008f2814e2f
Kartik Khorwal <hellokartikk@gmail.com>
2023-06-15 02:36:17 +0530

    remove:dock

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -8,14 +8,14 @@ import {
 } from "framer-motion";
 import { useRef } from "react";
 import { CircleStackIcon, CloudIcon, ComputerDesktopIcon, CpuChipIcon, CubeIcon, FolderIcon, ServerStackIcon, WindowIcon } from '@heroicons/react/24/outline';
-import PlaygroundContext from '../../src/context/Filecontext'
+// import PlaygroundContext from '../../src/context/Filecontext'
 
 interface DockProps {
     setIcon: (id: number) => void
 }
 
 function Dock({ setIcon }: DockProps) {
-    const {activeNodeIcon}:PlaygroundProps = useContext(PlaygroundContext)
+    // const {activeNodeIcon}:PlaygroundProps = useContext(PlaygroundContext)
     const [activeNode, setActiveNode] = useState<number | null>(null)
     const svgClass = "w-[50%] stroke-1"
 
@@ -32,9 +32,9 @@ function Dock({ setIcon }: DockProps) {
     let mouseX = useMotionValue(Infinity);
 
 
-    useEffect(() => {
-        setActiveNode(activeNodeIcon)
-    }, [activeNodeIcon])
+    // useEffect(() => {
+    //     setActiveNode(activeNodeIcon)
+    // }, [activeNodeIcon])
 
     const activateNode = (id: number) => {
         setActiveNode(id)
